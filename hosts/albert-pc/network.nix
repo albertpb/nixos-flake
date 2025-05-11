5854984e9c26b807df2ef123e37bb94536877034
albertpb <palumbo.alb@gmail.com>
2025-05-10 20:58:32 -0400

    fix vms networking

--- hosts/albert-pc/network.nix
+++ hosts/albert-pc/network.nix
@@ -1,9 +1,38 @@
 { pkgs, username, ... }:
 {
-  networking.networkmanager = {
-    enable = true;
-  };
+  networking = {
+    networkmanager = {
+      enable = true;
+    };
 
+    #interfaces.eno2.ipv4.addresses = [
+    #  {
+    #    address = "192.168.50.90";
+    #    prefixLength = 24;
+    #  }
+    #];
+
+    defaultGateway = {
+      address = "192.168.50.1";
+      interface = "br0";
+    };
+
+    nameservers = [ "192.168.50.99" ];
+    
+    bridges = {
+      "br0" = {
+        interfaces = [ "eno2" ];
+      };
+    };
+
+    interfaces.br0.ipv4.addresses = [
+      {
+        address = "192.168.50.90";
+        prefixLength = 24;
+      }
+    ];
+  }; 
+  
   users.users.${username}.packages = with pkgs; [
     networkmanagerapplet
   ];
