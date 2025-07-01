4cd84458fc41ada78f66ae0054a049b94460863d
albertpb <palumbo.alb@gmail.com>
2025-06-30 20:12:24 -0400

    fix nixos-tv nvidia

--- hosts/nixos-tv/nvidia.nix
+++ hosts/nixos-tv/nvidia.nix
@@ -3,13 +3,14 @@
     enable = true;
     enable32Bit = true;
     
-    extraPackages = [ pkgs.mesa.drivers ];
+    extraPackages = [ pkgs.mesa ];
   };
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
+    open = false;
     modesetting.enable = true;
 
     # Enable the Nvidia settings menu,
@@ -19,6 +20,6 @@
     nvidiaPersistenced = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
