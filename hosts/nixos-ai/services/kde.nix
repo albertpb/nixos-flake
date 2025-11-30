a8e6b8b8637d9f5ebc56733252406c744f943e3d
albertpb <palumbo.alb@gmail.com>
2025-11-30 16:41:43 -0300

    fix vivaldi open file portal

--- hosts/nixos-ai/services/kde.nix
+++ hosts/nixos-ai/services/kde.nix
@@ -1,10 +1,26 @@
-{ pkgs, ... }:
-{
-    services = {
-        desktopManager.plasma6.enable = true;
+{ pkgs, username, ... }: {
+  services = {
+    desktopManager.plasma6.enable = true;
 
-        displayManager.sddm.enable = true;
+    displayManager.sddm.enable = true;
 
-        displayManager.sddm.wayland.enable = true;
-    };
+    displayManager.sddm.wayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-wlr
+      pkgs.kdePackages.xdg-desktop-portal-kde
+    ];
+  };
+
+  users.users.${username}.packages = with pkgs; [
+    xdg-desktop-portal-hyprland
+    kdePackages.xdg-desktop-portal-kde
+    xdg-desktop-portal-wlr
+  ];
+
 }
--- modules/core/configuration.nix
+++ modules/core/configuration.nix
@@ -3,6 +3,8 @@
   environment = {
     variables = {
       EDITOR = "nvim";
+      NIXOS_OZONE_WL = "1";
+      GTK_USE_PORTAL = "1";
     };
   };
 }
