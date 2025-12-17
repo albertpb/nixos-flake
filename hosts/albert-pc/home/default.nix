cfdb045d5fd7a86208760d8b2d2d3162447ef0fa
albertpb <palumbo.alb@gmail.com>
2025-12-17 17:26:31 -0300

    remove pkgs albert-pc

--- hosts/albert-pc/home/default.nix
+++ hosts/albert-pc/home/default.nix
@@ -3,10 +3,8 @@
   imports = [
     (import ./../../../modules/core/programs)
   ] ++ [ (import ./hyprland.nix) ]
-  ++ [ (import ./vscodium.nix) ]
   ++ [ (import ./rofi.nix) ]
   ++ [ (import ./waybar.nix) ]
-  ++ [ (import ./keeweb) ]
   ++ [ (import ./git.nix) ]
   ++ [ (import ./mako.nix) ]
   ++ [ (import ./bashrc.nix) ];
--- hosts/albert-pc/services/udev.nix
+++ hosts/albert-pc/services/udev.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  # Keyboard to change settings in browser with hid
   services.udev.extraRules = ''
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0a31", MODE="0666", GROUP="plugdev"
   '';  
