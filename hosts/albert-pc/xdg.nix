cec537ddf990104aea0d6d304c232c0d1dd0d30c
albertpb <palumbo.alb@gmail.com>
2025-12-24 17:59:14 -0300

    fix xdg cosmic

--- hosts/albert-pc/xdg.nix
+++ hosts/albert-pc/xdg.nix
@@ -4,12 +4,12 @@
     enable = true;
     xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-cosmic
+      xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
       xdg-desktop-portal-wlr
     ];
 
-    config.common.default = "cosmic";
+    config.common.default = "gtk";
   };
 
 }
