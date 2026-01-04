932f5e64bbb732e39f7825fd6d80dfbf2865a757
albertpb <palumbo.alb@gmail.com>
2026-01-04 18:02:48 -0300

    move security to core

--- modules/core/home/keyring.nix
+++ modules/core/home/keyring.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   services.gnome-keyring.enable = true;
   home.packages = [ pkgs.gcr ];
 }
--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   security = {
     polkit.enable = true;
     rtkit.enable = true;
   };
+
+  security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
 }
