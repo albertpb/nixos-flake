b37a84f302dfacc2e03da920dd9e1ccef448f68b
albertpb <palumbo.alb@gmail.com>
2026-01-23 21:56:34 -0300

    add nixos-ai monitor alias

--- hosts/nixos-ai/home/zsh.nix
+++ hosts/nixos-ai/home/zsh.nix
@@ -1,8 +1,19 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   programs.zsh = {
-    initContent = '' 
+    initContent = ''
       export HF_HOME=/mnt/ai1/Huggingface
     '';
+
+    shellAliases = {
+      "4k" = "hyprctl keyword monitor ',3840x2160@240,0x0,1'";
+      "2k" = "hyprctl keyword monitor ',2560x1440@240,0x0,1'";
+    };
+
   };
 }
