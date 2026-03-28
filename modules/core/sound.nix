9c65ee8151ff38c9090b2b48b02c676808bc44d4
albertpb <palumbo.alb@gmail.com>
2026-03-28 04:17:13 -0300

    improve sound

--- modules/core/sound.nix
+++ modules/core/sound.nix
@@ -2,38 +2,43 @@
 let
   json = pkgs.formats.json { };
   pw_rnnoise_config = {
-    "context.modules" = [{
-      "name" = "libpipewire-module-filter-chain";
-      "args" = {
-        "node.description" = "Noise Canceling source";
-        "media.name" = "Noise Canceling source";
-        "filter.graph" = {
-          "nodes" = [{
-            "type" = "ladspa";
-            "name" = "rnnoise";
-            "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-            "label" = "noise_suppressor_stereo";
-            "control" = {
-              "VAD Threshold (%)" = 50.0;
-              "VAD Grace Period (ms)" = 200;
-              "Retroactive VAD Grace (ms)" = 0;
-            };
-          }];
+    "context.modules" = [
+      {
+        "name" = "libpipewire-module-filter-chain";
+        "args" = {
+          "node.description" = "Noise Canceling source";
+          "media.name" = "Noise Canceling source";
+          "filter.graph" = {
+            "nodes" = [
+              {
+                "type" = "ladspa";
+                "name" = "rnnoise";
+                "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                "label" = "noise_suppressor_stereo";
+                "control" = {
+                  "VAD Threshold (%)" = 50.0;
+                  "VAD Grace Period (ms)" = 200;
+                  "Retroactive VAD Grace (ms)" = 0;
+                };
+              }
+            ];
+          };
+          "capture.props" = {
+            "node.name" = "capture.rnnoise_source";
+            "node.passive" = true;
+            "audio.rate" = 48000;
+          };
+          "playback.props" = {
+            "node.name" = "audio.rate = 48000";
+            "media.class" = "Audio/Source";
+            "audio.rate" = 48000;
+          };
         };
-        "capture.props" = {
-          "node.name" = "capture.rnnoise_source";
-          "node.passive" = true;
-          "audio.rate" = 48000;
-        };
-        "playback.props" = {
-          "node.name" = "audio.rate = 48000";
-          "media.class" = "Audio/Source";
-          "audio.rate" = 48000;
-        };
-      };
-    }];
+      }
+    ];
   };
-in {
+in
+{
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -43,12 +48,31 @@ in {
     audio.enable = true;
     jack.enable = true;
 
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
+        alsa_monitor.rules = {
+          {
+            matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+            apply_properties = {
+              ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+              ["api.alsa.period-size"] = 512, -- defaults to 1024, tweak by trial-and-error
+            },
+          },
+        }
+      '')
+    ];
+
     wireplumber.extraConfig = {
       "monitor.bluez.properties" = {
         "bluez5.enable-sbc-xq" = true;
         "bluez5.enable-msbc" = true;
         "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
       };
     };
 
@@ -56,26 +80,28 @@ in {
       pipewire."92-low-latency.conf" = {
         context.properties = {
           default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 32;
+          default.clock.quantum = 512;
+          default.clock.min-quantum = 128;
+          default.clock.max-quantum = 512;
         };
       };
 
       pipewire-pulse."92-low-latency" = {
-        context.modules = [{
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            pulse.min.req = "32/48000";
-            pulse.default.req = "32/48000";
-            pulse.max.req = "32/48000";
-            pulse.min.quantum = "32/48000";
-            pulse.max.quantum = "32/48000";
-          };
-        }];
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "128/48000";
+              pulse.default.req = "128/48000";
+              pulse.max.req = "512/96000";
+              pulse.min.quantum = "128/48000";
+              pulse.max.quantum = "512/48000";
+            };
+          }
+        ];
         stream.properties = {
-          node.latency = "32/48000";
-          resample.quality = 1;
+          node.latency = "128/48000";
+          resample.quality = 2;
         };
       };
 
