866b01eb73fdd0090bf6a3ebb27e2bfb8da2b7e2
albertpb <palumbo.alb@gmail.com>
2026-09-05 17:16:12 -0400

    add steam to albert-pc

--- hosts/albert-pc/program.nix
+++ hosts/albert-pc/program.nix
@@ -1,6 +1,13 @@
 { inputs, pkgs, ... }: {
   imports = [ inputs.dms.nixosModules.dank-material-shell ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
   programs.hyprland = {
     enable = true;
     withUWSM = true;
