439ca2ad70f605bfdbfefa354c87e8b25c27a0ed
albertpb <palumbo.alb@gmail.com>
2026-09-06 18:02:27 -0300

    remove steam

--- hosts/albert-pc/program.nix
+++ hosts/albert-pc/program.nix
@@ -1,13 +1,6 @@
 { inputs, pkgs, ... }: {
   imports = [ inputs.dms.nixosModules.dank-material-shell ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-  };
-
   programs.hyprland = {
     enable = true;
     withUWSM = true;
