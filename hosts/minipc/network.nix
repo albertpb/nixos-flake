b2af90020b95a255ce7cb791dd2c1fec6844bc50
albertpb <palumbo.alb@gmail.com>
2026-09-07 22:23:26 -0300

    add minipc missing ports

--- hosts/minipc/network.nix
+++ hosts/minipc/network.nix
@@ -22,8 +22,8 @@
 
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 9443 8088 5432 445 139 ];
-      allowedUDPPorts = [ 443 7359 137 138 ];
+      allowedTCPPorts = [ 22 80 443 9443 8088 5432 6881 49494 445 139 ];
+      allowedUDPPorts = [ 443 7359 6881 1900 137 138 ];
       trustedInterfaces = [
         "docker0"
       ];
